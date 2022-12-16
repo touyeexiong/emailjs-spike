ad4048b9c1af4774ecf4e79a88639438829b079f
Tou Xiong <touyeexiong@gmail.com>
2022-12-15 20:46:23 -0600

    added emailjs code along with form for name, email, and message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function App() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "YOUR_SERVICE_ID",
+      "YOUR_TEMPLATE_ID",
+      form.current,
+      "YOUR_USER_ID"
+    ).then(
+      result => console.log(result.text),
+      error => console.log(error.text)  
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" required></input>
+        <label>Email</label>
+        <input type="email" name="user_email" required></input>
+        <label>Message</label>
+        <textarea name="message" required />
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
 }
